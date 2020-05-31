f3c81a08087a2c225ad2b74fec86cae8fffc9ddd
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-31 23:05:55 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,29 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  while i < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    couponed_item_name = "#{coupons[i][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    binding.pry
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon
+        
+      end
+    end
+    
+    binding.pry
+    i += 1
+  end  
 end
+
+
+
+
+
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
