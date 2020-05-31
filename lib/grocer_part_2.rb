b043e5ecb4e4c140a1e930e22067d3b1d0f5424d
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-31 23:33:38 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -39,7 +39,7 @@ def apply_clearance(cart)
     if item_hash[:clearance] == true
       new_price = {
         :item => item_hash[:item],
-        :price => item_hash[:price] - (item_hash[:price] * 0.2)
+        :price => item_hash[:price] - (item_hash[:price] * 0.20)
       }
       
       binding.pry
