6e83d55162389b19426690f7d2f3429499548096
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-31 23:48:13 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -39,8 +39,8 @@ def apply_clearance(cart)
   while i < cart.length
     if cart[i][:clearance] == true
       cart[i][:price] = (cart[i][:price] - (cart[i][:price] * .20)).round(2)
-      
-    ebinding.prynd
+      #binding.pry
+    end
     i += 1
   end
   cart
