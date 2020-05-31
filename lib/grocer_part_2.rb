ea56318073046a40814fc6e9f527c398aed354c1
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-31 23:13:59 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -15,7 +15,7 @@ def apply_coupons(cart, coupons)
       else 
         cart_item_with_coupon = {
           :item => couponed_item_name,
-          :price => coupons[i][:cost] / 
+          :price => coupons[i][:cost] / coupons[i][]
           
         }
       end
