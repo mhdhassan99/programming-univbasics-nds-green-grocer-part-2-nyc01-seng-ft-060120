605b08f3aecf6891d23ce1a5ee42f6b248685e37
Muhammad Hassan <mhassan.work99@gmail.com>
2020-06-01 00:15:38 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,76 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  while i < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    couponed_item_name = "#{coupons[i][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[i][:num]
+        cart_item[:count] -= coupons[i][:num]
+      else 
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[i][:cost] / coupons[i][:num],
+          :count => coupons[i][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item [:count] -= coupons[i][:num]
+      end
+    end
+    i += 1
+  end 
+  cart
 end
+
+
+
+
+
+
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  while i < cart.length
+    if cart[i][:clearance] == true
+      cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.20)).round(2)
+    end
+    i += 1
+  end
+  cart
 end
 
+
+
+
+
+
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+ consolidated_cart = consolidate_cart(cart)
+ couponed_cart = apply_coupons(consolidated_cart, coupons)
+ final_cart = apply_clearance(couponed_cart)
+ #binding.pry
+ total_price = 0
+ 
+ i = 0
+ while i < final_cart.length
+  total_price += final_cart[i][:price] * final_cart[i][:count]
+  i += 1
+ end
+  if total_price > 100
+    total_price -= (total_price * 0.10)
+  end
+  total_price
 end
+
+
+
+
+
+
+
