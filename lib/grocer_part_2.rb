2475025ae1d017009445ced983ac42f97416f239
Muhammad Hassan <mhassan.work99@gmail.com>
2020-06-01 00:05:39 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -53,5 +53,6 @@ end
 def checkout(cart, coupons)
  consolidated_cart = consolidate_cart(cart)
  couponed_cart = apply_coupons(consolidate_cart, coupons)
- final_cart = 
+ final_cart = apply_coupons(couponed_cart)
+ 
 end
